f8f504bebef26ab99847fafdfedff53adec0d1e1
Ajin Frank Justin <ajinfrankj@gmail.com>
2025-11-25 17:06:16 -0500

    assignment 6 final changes

--- Kambaz/Enrollments/dao.js
+++ Kambaz/Enrollments/dao.js
@@ -1,14 +1,17 @@
 import { v4 as uuidv4 } from "uuid";
 import model from "./model.js";
+import UserModel from "../Users/model.js";
 
 export default function EnrollmentsDao(db) {
   async function findCoursesForUser(userId) {
-    const enrollments = await model.find({ user: userId }).populate("course");
+    const enrollments = await model.find({ user: userId });
     return enrollments.map((enrollment) => enrollment.course);
   }
   async function findUsersForCourse(courseId) {
-    const enrollments = await model.find({ course: courseId }).populate("user");
-    return enrollments.map((enrollment) => enrollment.user);
+    const enrollments = await model.find({ course: courseId });
+    const userIds = enrollments.map((enrollment) => enrollment.user);
+    const users = await UserModel.find({ _id: { $in: userIds } });
+    return users;
   }
   async function findEnrollmentsForUser(userId) {
     return await model.find({ user: userId });
